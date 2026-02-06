1019ab51f1c29b8116942dc2257630eda7ef71d6
Vishal Jangid <vishaljangid80550786@gmail.com>
2026-02-06 13:19:00 +0530

    terminal done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import {Navbar,Welcome} from '#components'
+import {Draggable} from "gsap/Draggable";
+import gsap from 'gsap'
 
+import {Dock, Navbar, Welcome} from '#components'
+import {Terminal} from "#windows/index.js";
+
+
+gsap.registerPlugin(Draggable);
 
 const App = () => {
-  return (
-      <main>
-        <Navbar />
-          <Welcome />
-      </main>
-  )
+    return (
+        <main>
+            <Navbar/>
+            <Welcome/>
+            <Dock/>
+            <Terminal/>
+        </main>
+    )
 };
 
 export default App;
